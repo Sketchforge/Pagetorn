1376164c320a90833eceaae9dc4056b4f8d4f26d
Daniel <dancoches@gmail.com>
2023-02-28 00:38:32 -0600

    Started adding functionality to data reactor

--- Assets/Scripts/Events/PostProcessingEvent.cs
+++ Assets/Scripts/Events/PostProcessingEvent.cs
@@ -19,9 +19,11 @@ public class PostProcessingEvent : Event
     [SerializeField] private AnimationCurve _fadeIn = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
     [SerializeField] private float _duration = 4;
     [SerializeField] private AnimationCurve _fadeOut = new AnimationCurve(new Keyframe(0, 1), new Keyframe(1, 0));
+    public float _currDuration;
 
     public override void ActivateEvent()
     {
+        _currDuration = _duration;
         if (_affectFog)
         {
             GameManager.PostProcessingManager.SetFog(_fogDensity, _fogColor, _fadeIn, _duration, _fadeOut);
