22936130f6b713867fd68fcc16e56a40ab322432
Brandon Coffey <bmc939@gmail.com>
2023-03-29 03:52:13 -0500

    Alternative Room Generation Method

---
+++ Assets/Scripts/Level/SimplePlayerTrigger.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class SimplePlayerTrigger : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _onPlayerEnter;
+    [SerializeField] private UnityEvent _onPlayerLeave;
+    [SerializeField, ReadOnly] private bool _playerInside;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovementScript>())
+        {
+            _playerInside = true;
+            _onPlayerEnter.Invoke();
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerMovementScript>())
+        {
+            _playerInside = false;
+            _onPlayerLeave.Invoke();
+        }
+    }
+}
