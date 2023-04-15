81bc39165ac0590e021c63561cea0daeff87023a
Loansome <gabesalomone@gmail.com>
2023-04-15 17:48:04 -0500

    Fixed movement and spell cast bug
    Moving left, right, and forward should now all work as intended on all framerates, and the spells can now all be cast ahead of the player instead of being launched to the sides of the player.

--- Assets/Scripts/Player/Movement/PlayerMovementScript.cs
+++ Assets/Scripts/Player/Movement/PlayerMovementScript.cs
@@ -147,8 +147,6 @@ public class PlayerMovementScript : MonoBehaviour
         if (_viewDirection.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(_viewDirection.x, _viewDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
