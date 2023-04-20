8c1e2fb585d38389ebf83e0b57f7a323cec09163
Loansome <gabesalomone@gmail.com>
2023-04-20 14:16:31 -0500

    Performance increase with model combining
    Added script to combine the book models into one mesh during runtime. Some leftover bugs are that books may be off the shelf sometimes, usually if the shelf is rotated, or static books may not be there at all.
    To switch back to the individual models, you can go into the BookShelf_v1 prefab and switch the active state of the Static Books and Combined Books.

---
+++ Assets/Scripts/Rendering/MeshCombiner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCombiner : MonoBehaviour
+{
+    [SerializeField] private List<MeshFilter> sourceMeshFilters;
+    [SerializeField] private MeshFilter targetMeshFilter;
+
+	private void OnEnable()
+	{
+        CombineMeshes();
+	}
+
+	[Button]
+    private void CombineMeshes()
+	{
+        var combine = new CombineInstance[sourceMeshFilters.Count];
+
+        for (var i = 0; i< sourceMeshFilters.Count; i++)
+		{
+            combine[i].mesh = sourceMeshFilters[i].sharedMesh;
+            combine[i].transform = sourceMeshFilters[i].transform.localToWorldMatrix;
+		}
+
+        targetMeshFilter.mesh = new Mesh();
+        targetMeshFilter.mesh.CombineMeshes(combine);
+
+        transform.rotation = Quaternion.identity;
+        transform.localScale = new Vector3(1, 0.87f, 1.22f);
+        transform.position = Vector3.zero;
+	}
+}
