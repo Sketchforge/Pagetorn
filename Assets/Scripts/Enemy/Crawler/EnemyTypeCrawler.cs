414624767723cc5161aa13b1418fba5486fe0c9a
Daniel <dancoches@gmail.com>
2023-04-28 00:24:00 -0500

    [Do not merge] weird split in code, may be missed push

--- Assets/Scripts/Enemy/Crawler/EnemyTypeCrawler.cs
+++ Assets/Scripts/Enemy/Crawler/EnemyTypeCrawler.cs
@@ -27,8 +27,6 @@ public class EnemyTypeCrawler : EnemyBase
     [SerializeField, ReadOnly] private float _roamTime;
     [SerializeField] private bool hasRoamPos = true;
     private bool _themePlaying = false;
-    private bool _playedWalkingSound = false;
-  //  private float _moveSoundLength;
 
    
 
@@ -44,7 +42,6 @@ public class EnemyTypeCrawler : EnemyBase
 
         _myAnimator = _BetaFace.GetComponent<Animator>();
         UpdateClipLengths();
-      //  _moveSoundLength = _moveSound.Clip.length;
 
        // _musicPlayer = GameObject.FindGameObjectWithTag("MusicPlayer").GetComponent<AudioSource>();
     }
@@ -101,35 +98,24 @@ public class EnemyTypeCrawler : EnemyBase
     {
         //Debug.Log("RoamState");
         _memoryTime = Time.time;
-         
         if (Vector3.Distance(transform.position, _roamPosition) < 20f)
         {
             _roamPosition = GetRoamingPosition();
         }
         FacePosition(_roamPosition);
         MoveTo(_roamPosition);
+        //if (_moveSound.Clip.length - Time.deltaTime > 0)
+
+        if (_moveSound.Clip.length < Time.deltaTime)
+        {
+
+        }
+        _moveSound.PlayAtParentAndFollow(this.gameObject.transform);
         if (Vector3.Distance(transform.position, _roamPosition) < 0.5f || (HasAlpha && Vector3.Distance(transform.position, _alpha.transform.position) > 10f))
         {
             //Debug.Log("RoamStateIsBeingGlitchy");
             _roamPosition = GetRoamingPosition();
         }
-
-        if (!_playedWalkingSound)
-        {
-            _moveSound.PlayAtParentAndFollow(this.transform);
-            _playedWalkingSound = true;
-
-        }
-        //else if (_playedWalkingSound)
-        //{
-        //    _moveSoundLength -= Time.time;
-        //    if (_moveSoundLength <= 0)
-        //    {
-        //        _playedWalkingSound = false;
-        //        _moveSoundLength = _moveSound.Clip.length;
-        //    }
-        //        
-        //}
 
         if (CheckTarget()) TrySetState(CrawlerState.Chasing);
     }
@@ -156,15 +142,7 @@ public class EnemyTypeCrawler : EnemyBase
         }
 
         MoveTo(_target.transform.position + new Vector3(_randomFollowRange.x/2,0, _randomFollowRange.y/2));
-
-        if (!_playedWalkingSound)
-        {
-            _moveSound.PlayAtParentAndFollow(this.transform);
-            _playedWalkingSound = true;
-
-        }
-
-
+        _moveSound.PlayAtParentAndFollow(this.transform);
         if (_isAlpha)
             makeFollowersCircleTarget();
 
@@ -185,11 +163,10 @@ public class EnemyTypeCrawler : EnemyBase
             //    _musicPlayer.clip = _myTheme;
             //    _musicPlayer.Play();
             //}
-            if (!_themePlaying && !DataManager._chaseSongPlaying)
+            if (!_themePlaying)
             {
                 _chaseMusic.ActivateEvent();
                 _themePlaying = true;
-                DataManager._chaseSongPlaying = true;
             }
             
 
