09ca026318cea3b63392819bda949f7c4d0245bc
Loansome <gabesalomone@gmail.com>
2023-05-04 16:03:06 -0500

    Magic Level Up system/UI update
    Magic now has defined levels that they can go between, and can be seen in game. Can only really be seen and used by the PlayerStats object and the new UI script, so some items may need to be adjusted to fit these levels. (Plan for the future is to use an enum to tell what each level is supposed to be, so books and UI only need to know the level and not the point values)

---
+++ Assets/Scripts/Player/Magic/MagicLevelEnum.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Set these values to their respective levels so they can be selected for items like books, instead of inputting the point values themselves
+public enum MagicLevelEnum
+{
+    LVL1,
+    LVL2,
+    LVL3,
+    LVL4,
+    LVL5,
+    LVL6,
+    LVL7,
+    LVL8,
+    LVL9,
+    LVL10,
+}
---
+++ Assets/Scripts/UI/MagicStatDisplay.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MagicStatDisplay : MonoBehaviour
+{
+    [SerializeField] private Survival _survival;
+	[SerializeField] private SurvivalStatEnum _magicStat;
+    
+	[Header("References")]
+	[SerializeField] private TMP_Text _displayText;
+	[SerializeField] private Slider _slider;
+	[SerializeField] private CircleSlider _circleSlider;
+	
+	private float _max = 100;
+
+    // TODO: Functionality for when stats are low (_survival.IsStatLow(_stat))
+    
+    private void OnEnable()
+    {
+        _survival.OnStatsChanged += UpdateStat;
+        UpdateStat();
+    }
+
+    private void OnDisable()
+    {
+        _survival.OnStatsChanged -= UpdateStat;
+    }
+
+    private void Start()
+	{
+		_max = _survival.GetStatMax(_magicStat);
+		if (_slider) _slider.maxValue = _max;
+    }
+
+    private void UpdateStat()
+    {
+        float value = _survival.GetStat(_magicStat);
+        float[] levelCaps = _survival.LevelCaps.ToArray();
+        float prevExp = 0;
+        float nextExp = 1;
+        float currentLevel = 1;
+
+        for (int i = 0; i < levelCaps.Length; i++)
+		{
+            nextExp = levelCaps[i];
+            if (value < nextExp)
+                break;
+            else
+			{
+                prevExp = levelCaps[i];
+                currentLevel++;
+			}
+		}
+
+        if (_slider)
+        {
+            _slider.maxValue = nextExp;
+            _slider.minValue = prevExp;
+            _slider.value = value;
+        }
+	    if (_circleSlider) _circleSlider.UpdateSlider(value / _max);
+        if (_displayText) _displayText.text = "LV " + currentLevel.ToString();
+    }
+}
